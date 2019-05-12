8a2870f2fe214f9f862e303d724082bb6c7fd28b
smahn <asdhgfasd1@gmail.com>
2019-05-12 19:44:26 +0900

    blog 연동

--- blog-frontend/src/pages/PostPage.js
+++ blog-frontend/src/pages/PostPage.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import PageTemplate from 'components/common/PageTemplate';
-import PostInfo from 'components/post/PostInfo';
-import PostBody from 'components/post/PostBody';
+import AskRemoveModalContainer from 'containers/modal/AskRemoveModalContainer';
 
-const PostPage = () => {
+import Post from 'containers/post/Post';
+import * as postActions from 'store/modules/post';
+import { bindActionCreators } from 'redux';
+
+
+const PostPage = ({ match }) => {
+  const { id } = match.params;
   return (
     <PageTemplate>
-      <PostInfo/>
-      <PostBody/>
+      <Post id={id}/>
+      <AskRemoveModalContainer/>
     </PageTemplate>
   );
 };
+
+
+PostPage.preload = (dispatch, params) => {
+  const { id } = params;
+  const PostActions = bindActionCreators(postActions, dispatch);
+  return PostActions.getPost(id);
+}
 
 export default PostPage;
