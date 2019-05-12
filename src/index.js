06b257e5b3662553264e08f554085273db01f894
smahn <asdhgfasd1@gmail.com>
2019-05-12 16:30:19 +0900

    파일 제거

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 
+const api = require('./api');
+
 const app = new Koa();
 const router = new Router();
 
-router.get('/', (ctx) => {
-    ctx.body = 'home';
-});
-router.get('/about', (ctx)=>{
-    ctx.body = 'Introduce';
-});
+router.use('/api', api.routes());
 
 app.use(router.routes()).use(router.allowedMethods());
 
